aa8677db3aa4582a226e0550184176faedeb2a6f
Pierre Wieser <pwieser@trychlos.org>
2023-06-20 10:03:57 +0200

    Update jquery dependency to allow latest jquery 3.x (fix #83)

--- package.js
+++ package.js
@@ -1,33 +1,36 @@
 Package.describe({
-  summary: 'Layout Manager for Blaze (works well with FlowRouter)',
-  version: '2.3.0',
-  git: 'https://github.com/kadirahq/blaze-layout',
-  name: "kadira:blaze-layout"
+    summary: 'Layout Manager for Blaze (works well with FlowRouter)',
+    version: '2.4.0-rc',
+    git: 'https://github.com/trychlos/pwix-blaze-layout',
+    name: "pwix:blaze-layout"
 });
 
-Package.onUse(function (api) {
-  configure(api);
-  api.export(['BlazeLayout']);
+Package.onUse( function( api ){
+    configure( api );
+    api.export([
+        'BlazeLayout'
+    ]);
 });
 
-Package.onTest(function(api) {
-  configure(api);
-  api.use('tinytest');
-  api.addFiles('tests/client/init.templates.html', 'client');
-  api.addFiles('tests/client/init.templates.js', 'client');
-  api.addFiles('tests/client/unit.js', 'client');
-  api.addFiles('tests/client/integration.js', 'client');
+Package.onTest( function( api ){
+    configure( api );
+    api.use( 'tinytest' );
+    api.addFiles( 'tests/client/init.templates.html', 'client' );
+    api.addFiles( 'tests/client/init.templates.js', 'client' );
+    api.addFiles( 'tests/client/unit.js', 'client' );
+    api.addFiles( 'tests/client/integration.js', 'client' );
 });
 
-function configure(api) {
-  api.versionsFrom('1.0');
-  api.use('blaze');
-  api.use('templating');
-  api.use('reactive-dict');
-  api.use('underscore');
-  api.use('jquery');
-  api.use('tracker');
+function configure( api ){
+    api.versionsFrom( '1.0' );
+    api.use( 'blaze' );
+    api.use( 'templating' );
+    api.use( 'reactive-dict' );
+    api.use( 'underscore' );
+    // from https://github.com/meteor/blaze/blob/master/packages/blaze/package.js#L9
+    api.use( 'jquery@1.11.9 || 3.0.0', { weak: true }); // should be a weak dep, by having multiple "DOM backends"
+    api.use( 'tracker' );
 
-  api.addFiles('lib/client/namespace.js', 'client');
-  api.addFiles('lib/client/layout.js', 'client');
+    api.addFiles( 'lib/client/namespace.js', 'client' );
+    api.addFiles( 'lib/client/layout.js', 'client' );
 }
